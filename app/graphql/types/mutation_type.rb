9abd2c0792dd56a5efd79e1ab41198d0f2d65d3c
Matthew M-B <matthewmacraebovell@outlook.com>
2019-12-01 16:54:27 -0500

    add school to user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -27,6 +27,29 @@ module Types
 
       user_of_request.courses << course_of_request
       user_of_request.save
+
+    end
+
+    field :add_school_to_user, Boolean, null: true do 
+      description "Add school to staff"
+      argument :user_id, Int, required: true
+      argument :school_id, Int, required: true
+    end
+    def add_school_to_user(user_id:, school_id:)
+    
+      user_of_request = User.find_by(id: user_id)
+      school_of_request = School.find_by(id: school_id)
+  
+      if user_of_request.nil?
+        raise GraphQL::ExecutionError, "User does not exist"
+      end
+
+      if school_of_request.nil?
+        raise GraphQL::ExecutionError, "school doesn't exist"
+      end
+
+      user_of_request.schools << school_of_request
+      user_of_request.save
       
     end
   end
