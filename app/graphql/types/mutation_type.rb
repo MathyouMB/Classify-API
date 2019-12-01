77bc0837033f969cb9f1d5cb55550d7c872d7935
Matthew M-B <matthewmacraebovell@outlook.com>
2019-12-01 14:24:23 -0500

    add course to user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -6,5 +6,28 @@ module Types
     def test_field
       "Hello World"
     end
+
+    field :add_course_to_user, Boolean, null: true do 
+      description "Add course to staff"
+      argument :user_id, Int, required: true
+      argument :course_id, Int, required: true
+    end
+    def add_course_to_user(user_id:, course_id:)
+    
+      user_of_request = User.find_by(id: user_id)
+      course_of_request = Course.find_by(id: course_id)
+  
+      if user_of_request.nil?
+        raise GraphQL::ExecutionError, "User does not exist"
+      end
+
+      if course_of_request.nil?
+        raise GraphQL::ExecutionError, "course doesn't exist"
+      end
+
+      user_of_request.courses << course_of_request
+      user_of_request.save
+      
+    end
   end
 end
